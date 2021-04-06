98db6ea9f0c0aee0295689b5b9707870936ca1c5
nhoxrongvip <phatlam.media.vis@gmail.com>
2021-04-05 22:35:41 -0400

    Fragments work with NavDrawer

---
+++ ThanhAssign44/src/main/java/thanh/lam/n01335598/DownloadFrag.java
@@ -0,0 +1,22 @@
+package thanh.lam.n01335598;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DownloadFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_download,container,false);
+
+
+        return v;
+    }
+}
--- ThanhAssign44/src/main/java/thanh/lam/n01335598/ThanhActivity.java
+++ ThanhAssign44/src/main/java/thanh/lam/n01335598/ThanhActivity.java
@@ -1,19 +1,23 @@
 //Thanh Phat Lam N01335598 CENG258 RNC
 package thanh.lam.n01335598;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 
-
-public class ThanhActivity extends AppCompatActivity {
+public class ThanhActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -25,19 +29,23 @@ public class ThanhActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.ThanhDrawerLayout);
+        NavigationView navView = findViewById(R.id.ThanhNavView);
+        navView.setNavigationItemSelectedListener(this);
         //Create "3-dash icon"
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.ThanhFragContainer, new HomeFrag()).commit(); // Default Fragment
+
+
     }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else{
+        } else {
             new AlertDialog.Builder(this)
                     .setTitle(R.string.app_name)
                     .setCancelable(false)
@@ -47,5 +55,27 @@ public class ThanhActivity extends AppCompatActivity {
                     .create()
                     .show();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment frag = null;
+        switch (item.getItemId()) {
+            case R.id.ThanhHomeFrag:
+                frag = new HomeFrag();
+                break;
+            case R.id.ThanhDownloadFrag:
+                frag = new DownloadFrag();
+
+                break;
+            case R.id.ThanhWebServiceFrag:
+                frag = new WebServiceFrag();
+                break;
+        }
+        item.setChecked(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.ThanhFragContainer, frag).commit();
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }

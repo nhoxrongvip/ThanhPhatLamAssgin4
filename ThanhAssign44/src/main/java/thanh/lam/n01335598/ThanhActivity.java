052e3224e5db8aaebccbcdb6fefa5b018120207c
nhoxrongvip <phatlam.media.vis@gmail.com>
2021-04-05 20:51:41 -0400

    Create Navigation Drawer

--- ThanhAssign44/src/main/java/thanh/lam/n01335598/ThanhActivity.java
+++ ThanhAssign44/src/main/java/thanh/lam/n01335598/ThanhActivity.java
@@ -1,15 +1,51 @@
 //Thanh Phat Lam N01335598 CENG258 RNC
 package thanh.lam.n01335598;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.os.Bundle;
 
+
+
 public class ThanhActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.ThanhToolBar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.ThanhDrawerLayout);
+        //Create "3-dash icon"
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setCancelable(false)
+                    .setMessage("Do you want to quit?")
+                    .setPositiveButton("Yes", (dialog, which) -> finish())
+                    .setNegativeButton("No", (dialog, which) -> dialog.cancel())
+                    .create()
+                    .show();
+        }
     }
 }

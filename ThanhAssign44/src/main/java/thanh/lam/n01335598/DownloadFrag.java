2bac7e8ec5c6eee9c0db4e23e2c0e40bfb108ec1
nhoxrongvip <phatlam.media.vis@gmail.com>
2021-04-15 20:58:28 -0400

    Final Commit

--- ThanhAssign44/src/main/java/thanh/lam/n01335598/DownloadFrag.java
+++ ThanhAssign44/src/main/java/thanh/lam/n01335598/DownloadFrag.java
@@ -3,25 +3,19 @@ package thanh.lam.n01335598;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -30,27 +24,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.squareup.picasso.Picasso;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import thanh.lam.n01335598.ImageAdapter;
 
 public class DownloadFrag extends Fragment {
 
@@ -58,13 +44,14 @@ public class DownloadFrag extends Fragment {
             "https://i.pinimg.com/564x/0b/ac/f6/0bacf62a4bd456d02d02c6b8a5c98f67.jpg",
             "https://wallpapercave.com/wp/wp2722822.jpg"
     };
-    Bitmap bm;
 
     View view;
-    ImageView background, spinner_image;
+    ImageView background;
     Button downloadButton;
     ImageAdapter imgAdapter;
+    Bitmap bm;
     int downloadLength;
+    ProgressDialog p;
 
 
     @Nullable
@@ -78,7 +65,20 @@ public class DownloadFrag extends Fragment {
         imgAdapter = new ImageAdapter(getActivity(), imageUrlList);
         imageSpinner.setAdapter(imgAdapter);
         downloadButton = (Button) v.findViewById(R.id.ThanhDownloadButton);
-        downloadButton.setOnClickListener(v1 -> down());
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            200);
+
+                }else{
+                    down();
+                }
+            }
+        });
+
 
 
         return v;
@@ -87,12 +87,10 @@ public class DownloadFrag extends Fragment {
     private void down() {
         DownloadImage downloadImage = new DownloadImage();
         try {
-            Bitmap bm = downloadImage.execute(imgAdapter.getImageURL(imageUrlList)).get();
+            bm = downloadImage.execute(imgAdapter.getImageURL(imageUrlList)).get();
             background = view.findViewById(R.id.ThanhDownloadFragBackground);
             background.setImageBitmap(bm);
 
-
-            //Permission for download is requested in the AsyncTask
             new SaveImage().execute(bm);
 
         } catch (ExecutionException e) {
@@ -107,58 +105,34 @@ public class DownloadFrag extends Fragment {
         URL link;
         HttpURLConnection urlConnection;
         InputStream in;
-        ProgressDialog p;
+        Bitmap bm;
 
-        private void showDialog() {
-            p = new ProgressDialog(getActivity());
-            p.setMessage("Downloading The Image");
-            p.setIndeterminate(false);
-            p.setTitle("Download the Image");
-            p.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            p.setIcon(R.drawable.downloading);
-            //p.setMax(100);
-
-            p.show();
-
-
-        }
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showDialog();
-
-        }
 
         @Override
         protected void onProgressUpdate(String... values) {
-            p.setProgress(Integer.parseInt(values[0]));
+           p.setProgress(Integer.parseInt(values[0]));
 
-        }
 
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            p.dismiss();
         }
 
         @Override
         protected Bitmap doInBackground(String... strings) {
             bm = null;
-
             try {
                 int count;
-                int total=0;
+                int total = 0;
                 link = new URL(strings[0]);//Get url
                 urlConnection = (HttpURLConnection) link.openConnection();//Open url
                 in = urlConnection.getInputStream();
+                bm = BitmapFactory.decodeStream(in);
+
                 urlConnection.connect();
                 downloadLength = urlConnection.getContentLength();
                 byte data[] = new byte[1024];
-                while((count = in.read(data))!= -1){
-                    total+=count;
+                while ((count = in.read(data)) != -1) {
+                    total += count;
                     publishProgress(""+((total*100)/downloadLength));
                 }
-
-                bm = BitmapFactory.decodeStream(in);
 
 
             } catch (MalformedURLException e) {
@@ -169,23 +143,40 @@ public class DownloadFrag extends Fragment {
             return bm;
         }
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            p = new ProgressDialog(getActivity());
+            p.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            p.setTitle("Downloading");
+            p.setMessage("Downloading message");
+            p.show();
 
+
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            p.dismiss();
+        }
     }
 
-    private class SaveImage extends AsyncTask<Bitmap, Void, Bitmap> {
-        ProgressDialog p;
 
+    private class SaveImage extends AsyncTask<Bitmap, Void, Bitmap> {
 
 
         @Override
         protected void onPreExecute() {
+
             super.onPreExecute();
 
         }
 
         @Override
         protected Bitmap doInBackground(Bitmap... bitmaps) {
+
             //Request permission in here
+
             return bm;
         }
 
@@ -197,6 +188,7 @@ public class DownloadFrag extends Fragment {
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             super.onPostExecute(bitmap);
+
             if (background != null) {
                 background.setImageBitmap(bitmap);
                 background.invalidate();
@@ -220,6 +212,7 @@ public class DownloadFrag extends Fragment {
 
                 //If Image exists, toast, display to background but not download again
                 if (output.exists()) {
+                    p.dismiss();
                     Toast.makeText(getActivity(), "Image already downloaded. Check you file", Toast.LENGTH_SHORT).show();
 
                 } else {
@@ -229,8 +222,6 @@ public class DownloadFrag extends Fragment {
 
                         //Save Image
                         bit.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-
-
 
                         //Save image to Gallery or Google photo
                         Intent i = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
@@ -249,16 +240,19 @@ public class DownloadFrag extends Fragment {
                             e.printStackTrace();
                         }
 
+
                     }
                 }
-
 
             }
         }
     }
-
-
 }
+
+
+
+
+
 
 
 
